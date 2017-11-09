ab8e636c3268d97148d85ce0d361240bca79fb9c
shirase <shirase55@gmail.com>
2017-11-09 09:59:20 +0300

    Bug: controller delete action lost returns for redirect

--- generators/crud/default/controller.php
+++ generators/crud/default/controller.php
@@ -224,7 +224,7 @@ class <?= $controllerClass ?> extends <?= StringHelper::basename($generator->bas
             Yii::$app->session->setFlash('script', '$(document).trigger("action.Delete", '.Json::encode(['class'=>$model::className()]+$model->attributes).');');
             $model->delete();
             if (!Yii::$app->request->isAjax) {
-                $this->redirect(['index', 'returned'=>true]);
+                return $this->redirect(['index', 'returned'=>true]);
             }
         }
     }
